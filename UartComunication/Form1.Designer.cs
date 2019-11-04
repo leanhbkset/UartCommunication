1a110102b08f259bf1539f723d692eda166e0bed
leanhbkset <leanhbkset@gmail.com>
2019-11-04 14:47:41 +0900

    uart

---
+++ UartComunication/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace ComputerToArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxStrength = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxSide = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxPattern = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDataSending = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Enabled = false;
+            this.btnConnect.Location = new System.Drawing.Point(6, 25);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(2);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(102, 28);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(112, 30);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(71, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(161, 102);
+            this.btnWrite.Margin = new System.Windows.Forms.Padding(2);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(90, 19);
+            this.btnWrite.TabIndex = 4;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.btnConnect);
+            this.groupBox2.Location = new System.Drawing.Point(9, 201);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(197, 58);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Serial Connection";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.comboBoxStrength);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.comboBoxSide);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.comboBoxPattern);
+            this.groupBox3.Controls.Add(this.btnWrite);
+            this.groupBox3.Location = new System.Drawing.Point(14, 24);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(412, 136);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Setup";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(311, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Strength";
+            // 
+            // comboBoxStrength
+            // 
+            this.comboBoxStrength.FormattingEnabled = true;
+            this.comboBoxStrength.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBoxStrength.Location = new System.Drawing.Point(271, 43);
+            this.comboBoxStrength.Name = "comboBoxStrength";
+            this.comboBoxStrength.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxStrength.TabIndex = 9;
+            this.comboBoxStrength.SelectedIndexChanged += new System.EventHandler(this.comboBoxStrength_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Side";
+            // 
+            // comboBoxSide
+            // 
+            this.comboBoxSide.FormattingEnabled = true;
+            this.comboBoxSide.Items.AddRange(new object[] {
+            "Left",
+            "Right"});
+            this.comboBoxSide.Location = new System.Drawing.Point(144, 43);
+            this.comboBoxSide.Name = "comboBoxSide";
+            this.comboBoxSide.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSide.TabIndex = 7;
+            this.comboBoxSide.SelectedIndexChanged += new System.EventHandler(this.comboBoxSide_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Pattern index";
+            // 
+            // comboBoxPattern
+            // 
+            this.comboBoxPattern.FormattingEnabled = true;
+            this.comboBoxPattern.Items.AddRange(new object[] {
+            "Pattern 1",
+            "Pattern 2",
+            "Pattern 3",
+            "Pattern 4",
+            "Pattern 5"});
+            this.comboBoxPattern.Location = new System.Drawing.Point(17, 43);
+            this.comboBoxPattern.Name = "comboBoxPattern";
+            this.comboBoxPattern.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPattern.TabIndex = 5;
+            this.comboBoxPattern.SelectedIndexChanged += new System.EventHandler(this.comboBoxPattern_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Data sending";
+            // 
+            // txtDataSending
+            // 
+            this.txtDataSending.AutoSize = true;
+            this.txtDataSending.Location = new System.Drawing.Point(183, 311);
+            this.txtDataSending.Name = "txtDataSending";
+            this.txtDataSending.Size = new System.Drawing.Size(0, 13);
+            this.txtDataSending.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 363);
+            this.Controls.Add(this.txtDataSending);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Uart Comunication";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxStrength;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxSide;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxPattern;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label txtDataSending;
+    }
+}
+
---
+++ UartComunication/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ComputerToArduino
+{
+    public partial class Form1 : Form
+
+    {
+        bool isConnected = false;
+        String[] ports;
+        SerialPort port;
+        byte[] data;
+        byte[] patterns = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05 };
+        byte[] side = new byte[] {0x01, 0x02 };
+        byte[] strength = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05 };
+
+        public Form1()
+        {
+            InitializeComponent();
+            disableControls();
+            getAvailableComPorts();
+
+            data = new byte[5];
+
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+                Console.WriteLine(port);
+                if (ports[0] != null)
+                {
+                    btnConnect.Enabled = true;
+                    comboBox1.SelectedItem = ports[0];
+                }
+                else
+                {
+                    btnConnect.Enabled = false;
+                }
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                connect();
+            } else
+            {
+                disconnect();
+            }
+        }
+
+        void getAvailableComPorts()
+        {
+            ports = SerialPort.GetPortNames();
+        }
+
+        private void connect()
+        {
+            data[0] = 0x43;
+            data[1] = 0x31;
+            data[2] = 0x31;
+            data[3] = 0x31;
+            data[4] = 0x00;
+
+            isConnected = true;
+            string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
+            port = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+            if (ports[0] != null)
+            {
+                port.Open();
+                //port.Write("#STAR\n");
+                btnConnect.Text = "Disconnect";
+                enableControls();
+            }
+        }
+
+
+        private void disconnect()
+        {
+            isConnected = false;
+            //port.Write("#STOP\n");
+            port.Close();
+            btnConnect.Text = "Connect";
+            disableControls();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                txtDataSending.Text = data[0].ToString("X") + " " + data[1].ToString("X") + " "
+                    + data[2].ToString("X") + " " + data[3].ToString("X") + " "
+                    + data[4].ToString("X");
+
+                port.Write(data, 0, data.Length);
+            }
+        }
+
+        private void enableControls()
+        {
+            comboBoxPattern.Enabled = true;
+            comboBoxSide.Enabled = true;
+            comboBoxStrength.Enabled = true;
+            btnWrite.Enabled = true;
+            groupBox3.Enabled = true;
+            comboBoxPattern.Text = "Pattern 1";
+            comboBoxSide.Text = "Left";
+            comboBoxStrength.Text = "1";
+
+        }
+
+        private void disableControls()
+        {
+            comboBoxPattern.Enabled = false;
+            comboBoxSide.Enabled = false;
+            comboBoxStrength.Enabled = false;
+            btnWrite.Enabled = false;
+            groupBox3.Enabled = false;
+            comboBoxPattern.Text = "";
+            comboBoxSide.Text = "";
+            comboBoxStrength.Text = "";
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = true;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            isConnected = false;
+            btnConnect.Text = "Connect";
+            disableControls();
+
+            disableControls();
+            getAvailableComPorts();
+
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+                Console.WriteLine(port);
+                if (ports[0] != null)
+                {
+                    btnConnect.Enabled = true;
+                    comboBox1.SelectedItem = ports[0];
+                }
+                else
+                {
+                    btnConnect.Enabled = false;
+                    comboBox1.Text = "";
+                }
+            }
+        }
+
+        private void comboBoxPattern_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedPatternIndex = comboBoxPattern.SelectedIndex;
+
+            switch (selectedPatternIndex)
+            {
+                case 0:
+                    data[1] = patterns[0];
+                    break;
+                case 1:
+                    data[1] = patterns[1];
+                    break;
+                case 2:
+                    data[1] = patterns[2];
+                    break;
+                case 3:
+                    data[1] = patterns[3];
+                    break;
+                case 4:
+                    data[1] = patterns[4];
+                    break;
+                default:
+                    data[1] = patterns[0];
+                    break;
+            }
+        }
+
+        private void comboBoxSide_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedSideIndex = comboBoxSide.SelectedIndex;
+            switch (selectedSideIndex)
+            {
+                case 0:
+                    data[2] = side[0];
+                    break;
+                case 1:
+                    data[2] = side[1];
+                    break;
+                default:
+                    data[2] = side[0];
+                    break;
+            }
+        }
+
+        private void comboBoxStrength_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedStrengthIndex = comboBoxStrength.SelectedIndex;
+            switch(selectedStrengthIndex)
+            {
+                case 0:
+                    data[3] = strength[0];
+                    break;
+                case 1:
+                    data[3] = strength[1];
+                    break;
+                case 2:
+                    data[3] = strength[2];
+                    break;
+                case 3:
+                    data[3] = strength[3];
+                    break;
+                case 4:
+                    data[3] = strength[4];
+                    break;
+                default:
+                    data[3] = strength[0];
+                    break;
+            }
+        }
+    }
+}
